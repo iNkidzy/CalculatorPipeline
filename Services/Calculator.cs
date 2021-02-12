9f065e36846396db4f57934eb163477e193c0ee0
Nadia Miteva <nadq.nadia@gmail.com>
2021-02-12 13:59:07 +0100

    method1

--- Services/Calculator.cs
+++ Services/Calculator.cs
@@ -5,9 +5,23 @@ namespace Services
 {
     public class Calculator : ICalculator
     {
+
         public double Add(params double[] numbers)
         {
-            throw new NotImplementedException();
+            total = 0;
+            if(numbers.Length = 0)
+            {
+                 Console.WriteLine("Numbers is empty!");
+            }
+            else
+            {
+                for(double i =0; i< numbers.Length; i++)
+                {
+                    Console.Write(numbers[i] + " ")
+                    total += numbers[i]
+                }
+            }
+            return total;
         }
 
         public double Subtract(params double[] numbers)
